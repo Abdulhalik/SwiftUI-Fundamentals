1fc1109203f28dfe29d0328905dc6d8600127b8a
Abdulhalik <abdlhlkkrkmz@gmail.com>
2022-04-28 01:17:56 +0300

    initial

---
+++ SwiftUI-Fundamentals/SwiftUI-Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI-Fundamentals
+//
+//  Created by Abdulhalık Korkmaz on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
